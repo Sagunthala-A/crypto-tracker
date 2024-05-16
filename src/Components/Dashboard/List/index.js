da303390cfc6e379d1b66e1c424a39e1ba9927b9
Sagunthala-A <sagunthala9925@gmail.com>
2024-05-16 11:42:27 +0530

    added tooltip to all td cells

--- src/Components/Dashboard/List/index.js
+++ src/Components/Dashboard/List/index.js
@@ -9,80 +9,78 @@ function List({ coin }) {
   return (
     <tr className="list__row">
       <td className="td__logo">
-        <img className="" src={coin.image} alt={`${coin.name}_logo`} />
+        <Tooltip title={`${coin.name}-image`} placement="bottom-start">
+          <img className="" src={coin.image} alt={`${coin.name}_logo`} />
+        </Tooltip>
       </td>
-      <td className="td__logoName">
-        <div className="td__logoNameInfo">
-          <h4>{coin.symbol}</h4>
-          <p>{coin.name}</p>
-        </div>
-      </td>
-      <td>
-        <div className="td__chip__flex">
-          <div
-            className={
-              coin.price_change_percentage_24h >= 0
-                ? "price__chip"
-                : "price__chip redChip"
-            }
-          >
-            {coin.price_change_percentage_24h.toFixed(2)}%
+      <Tooltip title="Coin-info" placement="bottom">
+        <td className="td__logoName">
+          <div className="td__logoNameInfo">
+            <h4>{coin.symbol}</h4>
+            <p>{coin.name}</p>
           </div>
-          <div
-            className={
-              coin.price_change_percentage_24h >= 0
-                ? "icon__chip"
-                : "icon__chip redChip"
-            }
-          >
-            {coin.price_change_percentage_24h >= 0 ? (
-              <TrendingUpRoundedIcon />
-            ) : (
-              <TrendingDownRoundedIcon />
-            )}
+        </td>
+      </Tooltip>
+      <Tooltip title="Price" placement="bottom">
+        <td>
+          <div className="td__chip__flex">
+            <div
+              className={
+                coin.price_change_percentage_24h >= 0
+                  ? "price__chip td__priceChip"
+                  : "price__chip td__priceChip redChip"
+              }
+            >
+              {coin.price_change_percentage_24h.toFixed(2)}%
+            </div>
+            <div
+              className={
+                coin.price_change_percentage_24h >= 0
+                  ? "icon__chip"
+                  : "icon__chip redChip"
+              }
+            >
+              {coin.price_change_percentage_24h >= 0 ? (
+                <TrendingUpRoundedIcon />
+              ) : (
+                <TrendingDownRoundedIcon />
+              )}
+            </div>
           </div>
-        </div>
-      </td>
-      <td
-        className={
-          coin.price_change_percentage_24h >= 0
-            ? "td__coin__currentPrice"
-            : "td__coin__currentPrice redCurrentPrice"
-        }
-      >
-        <p
-          className="td__coinCurrentPriceDesktop"
-          // onClick={() => {
-          //   console.log(convertNumber(coin.current_price));
-          // }}
+        </td>
+      </Tooltip>
+      <Tooltip title="Current" placement="bottom-start">
+        <td
+          className={
+            coin.price_change_percentage_24h >= 0
+              ? "td__coin__currentPrice"
+              : "td__coin__currentPrice redCurrentPrice"
+          }
         >
-          ${coin.current_price.toLocaleString()}
-        </p>
-        <p className="td__coinCurrentPriceMobile">
-          ${convertNumber(coin.current_price)}
-        </p>
-      </td>
-      <td
-        className="td__coin__valueContainer td__coinTotVol"
-        // onClick={() => {
-        //   console.log(convertNumber(coin.total_volume));
-        // }}
-      >
-        {coin.total_volume.toLocaleString()}
-      </td>
-      <td
-        className="td__coin__valueContainer"
-        // onClick={() => {
-        //   console.log(convertNumber(coin.market_cap));
-        // }}
-      >
-        <span className="td__coinMarkCap">
-          ${coin.market_cap.toLocaleString()}
-        </span>
-        <span className="td__coinMarkCapMobile">
-          ${convertNumber(coin.market_cap)}
-        </span>
-      </td>
+          <p className="td__coinCurrentPriceDesktop">
+            ${coin.current_price.toLocaleString()}
+          </p>
+
+          <p className="td__coinCurrentPriceMobile">
+            ${convertNumber(coin.current_price)}
+          </p>
+        </td>
+      </Tooltip>
+      <Tooltip title="Total Volume" placement="bottom-end">
+        <td className="td__coin__valueContainer td__coinTotVol">
+          {coin.total_volume.toLocaleString()}
+        </td>
+      </Tooltip>
+      <Tooltip title="Market Cap" placement="bottom-end">
+        <td className="td__coin__valueContainer">
+          <span className="td__coinMarkCap">
+            ${coin.market_cap.toLocaleString()}
+          </span>
+          <span className="td__coinMarkCapMobile">
+            ${convertNumber(coin.market_cap)}
+          </span>
+        </td>
+      </Tooltip>
       {/* <td className="star__bookmark">@%&*</td> */}
     </tr>
   );
